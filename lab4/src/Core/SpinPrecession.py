b8012cd34f867979e76a17b7c4b5cb56eaeb4a11
OrlowskiWojtek <owoj@vp.pl>
2025-05-20 20:35:55 +0200

    lab4 wip

--- lab4/src/Core/SpinPrecession.py
+++ lab4/src/Core/SpinPrecession.py
@@ -9,10 +9,9 @@ from . import Utils as utl
 class SpinSystem():
     u = utl.Utils()
 
-    # TODO: check default alpha value
     def __init__(self, *, dx = 4, L = int(500), W = int(25), m = 0.014
-                        , Bx = 0, By = 0, Bz = 0
-                        , alpha = 0.000):
+                        , Bx = 0., By = 0., Bz = 0.
+                        , alpha = 0.000, additional_By_boundaries = None, additional_By = 0.):
         self.dx = self.u.nm2au(dx)
         self.L = int(L)
         self.W = int(W)
@@ -20,29 +19,34 @@ class SpinSystem():
         self.Bx = self.u.T2au(Bx)
         self.By = self.u.T2au(By)
         self.Bz = self.u.T2au(Bz)
+
         self.alpha = self.u.nm2au(self.u.eV2au(alpha))
 
-        self.precalc_mag_term = 0.5 * self.u.bohr_magneton_au * self.u.lande_g * \
-                                       (self.Bx * self.u.sigma_x + 
+        self.precalc_mag_term = 0.5 * self.u.bohr_magneton_au * self.u.lande_g * (self.Bx * self.u.sigma_x + 
                                         self.By * self.u.sigma_y + 
                                         self.Bz * self.u.sigma_z)
 
         self.precalc_t = 1.0/(2.0*self.m * self.dx * self.dx)
         self.precalc_tso =  self.alpha / (2 * self.dx)
 
+        self.add_term = additional_By_boundaries
+        self.additional_By = self.u.T2au(additional_By)
+
 
     def make_system(self): 
         def onsite(site):
+            value = 4*self.precalc_t * np.identity(2) + self.precalc_mag_term
+            if(self.add_term != None):
+                if((site.pos[0] > self.add_term[0] * self.L * self.dx) and (site.pos[0] < self.add_term[1] * self.L * self.dx)):
+                    value += 0.5 * self.u.bohr_magneton_au * self.u.lande_g * (self.additional_By * self.u.sigma_y)
 
-            return 4*self.precalc_t * np.identity(2) + self.precalc_mag_term
+            return value
 
         def hopping_x(sitei, sitej):
-
             return -self.precalc_t * np.identity(2) + 1j * self.precalc_tso * self.u.sigma_y
         
         def hopping_y(sitei, sitej):
-
-             return -self.precalc_t * np.identity(2) - 1j * self.precalc_tso * self.u.sigma_x
+            return -self.precalc_t * np.identity(2) - 1j * self.precalc_tso * self.u.sigma_x
 
         sys = kwant.Builder()  
         lat = kwant.lattice.square(self.dx, norbs=2)
@@ -75,9 +79,69 @@ class SpinSystem():
     def dispersion(self, nr_lead, k_max, nk):
         sys = self.make_system()
         momenta = np.linspace(-k_max*self.dx,k_max*self.dx,nk)
-        bands=kwant.physics.Bands(sys.leads[nr_lead])
-        energies=[bands(k) for k in momenta]
+        bands = kwant.physics.Bands(sys.leads[nr_lead])
+        energies = [bands(k) for k in momenta]
         return (momenta/self.dx)*self.u.nm2au(1.0), energies
+
+    #calculates the transmission coefficient - input in a. u.
+    def _transmission(self, E):
+        sys= self.make_system()
+        smatrix=kwant.smatrix(sys,E)
+        t=smatrix.transmission(1,0) # total transmission from 0 to 1 (or reversed)
+        return t
+    
+    #calculates the conductance - the Landauer formula is used
+    def conductance(self, Emax, ne):
+        energies=np.linspace(0,Emax,ne)
+        cond=[self._transmission(self.u.eV2au(E)) for E in energies]
+        return energies, cond
+
+    def transmission(self, E, leads, spins):
+        E = self.u.eV2au(E)
+        sys= self.make_system()
+        smatrix=kwant.smatrix(sys,E)
+        t=smatrix.transmission((leads[0], spins[0]), (leads[1], spins[1]))
+        return t
+
+    density_up = np.array([[1,0],\
+    [0,0]])
+
+    density_down= np.array([[0,0],\
+    [0,1]])
+
+    density_both= np.array([[1,0],\
+    [0,1]])
+
+    def wave_function(self, E):
+        E = self.u.eV2au(E)
+        sys = self.make_system()
+        wave_f = kwant.wave_function(sys, E)(0)
+
+        density_up_op   =   kwant.operator.Density(sys, self.density_up)
+        density_down_op =   kwant.operator.Density(sys, self.density_down)
+        density_both_op =   kwant.operator.Density(sys, self.density_both)
+
+        density_up_map      =   density_up_op(wave_f[0])
+        density_down_map    =   density_down_op(wave_f[0])
+        density_both_map    =   density_both_op(wave_f[0])
+        
+        return (density_up_map, density_down_map, density_both_map, sys)
+
+    def current(self, E):
+        E = self.u.eV2au(E)
+        sys = self.make_system()
+        wave_f = kwant.wave_function(sys, E)(0)
+
+        density_up_op   =   kwant.operator.Current(sys, self.density_up)
+        density_down_op =   kwant.operator.Current(sys, self.density_down)
+        density_both_op =   kwant.operator.Current(sys, self.density_both)
+
+        density_up_map      =   density_up_op(wave_f[0])
+        density_down_map    =   density_down_op(wave_f[0])
+        density_both_map    =   density_both_op(wave_f[0])
+        
+        return (density_up_map, density_down_map, density_both_map, sys)
+
 
 ###########################################################################
 ##  Define various functions calculating the basic physical properties   ##
@@ -91,28 +155,6 @@ def transmission_reflection(nw, E):
     r=smatrix.transmission(0,0)
     t=smatrix.transmission(1,0)
     return r, t
-
-#calculates the transmission coefficient
-def transmission(nw, E):
-    E=eV2au(E)
-    sys=make_system(nw)
-    smatrix=kwant.smatrix(sys,E)
-    t=smatrix.transmission(1,0)
-    return t
-
-#calculates the conductance - the Landauer formula is used
-def conductance(nw, Emax, ne):
-    energies=np.linspace(0,Emax,ne)
-    cond=[transmission(nw, E) for E in energies]
-    return energies, cond
-
-#plots the wave function of an electron with energy E incident in the contact nr_lead
-def wave_function(nw, E, nr_lead, *, ax = None):
-    E=eV2au(E)
-    sys=make_system(nw)
-    wave=kwant.wave_function(sys, E)
-    density=(abs(wave(nr_lead))**2).sum(axis=0)
-    kwant.plotter.map(sys,density, ax = ax, dpi = 300)
 
 #fplots the dos of an electron with energy E
 def dos(nw, E):
--- lab4/src/Core/TaskManager.py
+++ lab4/src/Core/TaskManager.py
@@ -2,27 +2,71 @@ from . import SpinPrecession as spin_system
 import kwant as kw
 import numpy as np
 import Core.Utils as utl
-import matplotlib.pyplot as plt
+
+from . import GeneralPlotter as plot_utils
 
 u = utl.Utils()
+general_plotter = plot_utils.Plotter()
 
 class TaskManager():
     def task1(self, *, do_plot = False):
-        sp = spin_system.SpinSystem()
-        sys = sp.make_system()
-        kw.plot(sys, show = do_plot);
 
-        moments, enes = sp.dispersion(0, .1, 200)
-        plt.figure();
-        plt.plot(moments, np.asarray(enes) / u.eV2au(1.0),'k-')
-        plt.tick_params(axis='both', which='major', labelsize=22)
-        plt.ylim((0,.04))  
-        plt.xlim((-0.15,0.15))  
-        plt.xlabel("k [1/nm]",fontsize=22)
-        plt.ylabel("E [eV]",fontsize=22)
-        plt.tight_layout()
-        plt.savefig("../plots/disp_ex1.pdf")
-        plt.show()
+        #sp = spin_system.SpinSystem()
+        #sys = sp.make_system()
+        #kw.plot(sys, show = do_plot, file = "../plots/ex1_system.pdf");
+
+        #moments, enes = sp.dispersion(0, .05, 200)
+        #general_plotter.plot_dispersion(moments, enes, filename = "../plots/ex1_disp.pdf", show = False)
+
+        #sp = spin_system.SpinSystem(Bx = 1)
+        #moments, enes = sp.dispersion(0, .05, 200)
+        #general_plotter.plot_dispersion(moments, enes, filename = "../plots/ex1_disp_bx_1.pdf", show = False)
+
+        #sp = spin_system.SpinSystem(By = 1)
+        #moments, enes = sp.dispersion(0, .05, 200)
+        #general_plotter.plot_dispersion(moments, enes, filename = "../plots/ex1_disp_by_1.pdf", show = False)
+
+        #sp = spin_system.SpinSystem(Bz = 1)
+        #moments, enes = sp.dispersion(0, .05, 200)
+        #general_plotter.plot_dispersion(moments, enes, filename = "../plots/ex1_disp_bz_1.pdf", show = False)
+
+        #enes, conds = sp.conductance(0.05, 50)
+        #general_plotter.plot_conductance(conds, enes, filename = "../plots/ex1_conductance_bz_1.pdf", show = False)
+        
+        ## to run for night
+        #t_bys_values = np.linspace(0, 1, 50) 
+        #E = 0.005
+        #transmittances = np.zeros((4, len(t_bys_values)), dtype=np.float64)
+
+        #for idx, by in enumerate(t_bys_values):
+        #    sp = spin_system.SpinSystem(Bz = 0.1, additional_By = by, additional_By_boundaries=[0.3, 0.8])
+        #    t_upup = sp.transmission(E, [1, 0], [1, 1])
+        #    t_updown = sp.transmission(E, [1, 0], [0, 1])
+        #    t_downup = sp.transmission(E, [1, 0], [1, 0])
+        #    t_downdown = sp.transmission(E, [1, 0], [0, 0])
+        #    transmittances[0, idx] = t_upup
+        #    transmittances[1, idx] = t_updown
+        #    transmittances[2, idx] = t_downup
+        #    transmittances[3, idx] = t_downdown
+
+        #general_plotter.plot_transmittance(t_bys_values, transmittances, show = True, filename = "../plots/ex1_transmittances.pdf")
+        sp = spin_system.SpinSystem(Bz = 0.1, additional_By = 0.6, additional_By_boundaries=[0.3, 0.8])
+        sys = sp.make_system();
+        E = 0.005
+        up, down, both, sys = sp.wave_function(E)
+        general_plotter.plot_wavefunction(sys, up, show = True, filename = "ex1_density_up.pdf")
+        general_plotter.plot_wavefunction(sys, down, show = True, filename = "ex1_density_down.pdf")
+        general_plotter.plot_wavefunction(sys, both, show = True, filename = "ex1_density_both.pdf")
+
+        #up, down, both, sys = sp.current(E)
+        #general_plotter.plot_current(sys, up, show = True, filename = "ex1_current_up.pdf")
+        #general_plotter.plot_current(sys, down, show = True, filename = "ex1_current_down.pdf")
+        #general_plotter.plot_current(sys, both, show = True, filename = "ex1_current_both.pdf")
+
+        
+        
+
+        
 
 
         
